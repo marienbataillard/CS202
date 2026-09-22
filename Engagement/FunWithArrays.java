4c2e61121f1823d0c970924df8fcb963d54097e7
Mars <marie.n.bataillard@gmail.com>
2026-09-22 11:32:10 -0700

    Creation of engagement

---
+++ Engagement/FunWithArrays.java
@@ -0,0 +1,9 @@
+public class FunWithArrays {
+    
+    void main() {
+        String[] row1 = {"Thoai", "Karl", "Mars", "Kyosuke"};
+        for (var n : row1) {
+            IO.println(n)
+        }
+    }
+}
