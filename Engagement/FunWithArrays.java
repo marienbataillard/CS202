50f844da508c05746db5de2c57070b397c4b17d3
Mars <marie.n.bataillard@gmail.com>
2026-09-22 12:12:17 -0700

    added scors

--- Engagement/FunWithArrays.java
+++ Engagement/FunWithArrays.java
@@ -13,18 +13,29 @@ public class FunWithArrays {
             IO.println(n+"'s favourite class is CS202!");
         }
 
-        double[] quizScores = {100, 90, 80, 70, 65.7};
-        
+        //double[] quizScores = {100, 90, 80, 70, 65.7, 15.5, 98.6, 5.4};
+        String csv = "100,90,80,70,65.7,15.5,98.6,5.4";
+        String{} rexzina = csv.split(",");
+
+        row1[1] = "Mars";
+        row1[2] = "Karl";
+        Io.println"Switching Mars and Karl!)";
+        for (var n: row1) {
+            IO.println(n+"'s favourite class is CS202!");
+        }
+
         double avg = 0; 
         double sum = 0;
         for (var score : quizScores) {
-            IO.println ("processing score: " + score);
+            //IO.println ("processing score: " + score);
+            double shane = souble.pa
             sum = sum + score;
         }
-        avg = sum / quizScores.length;
+        avg = sum / rexzina.length;
         IO.println ("The average score is: " + avg);
     }
 }
 
 //Compile time bugs. 
 // Run-time  bugs;
+//CSV = "comma seperated values" 
