13234cfe9e8adddf1267f32cd82fc9121988156a
Mars <marie.n.bataillard@outlook.com>
2026-09-23 15:17:32 -1000

    addeded

--- Assignments/Theater.java
+++ Assignments/Theater.java
@@ -1,19 +1,28 @@
 import java.awt.Graphics;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
-public class Theater extends JPanel {
+public class Theater extends JPanel  {
 
+    int age; 
+    ImageIcon img;
     public Theater() {
         String input = JOptionPane.showInputDialog("What is your age?");
+        age = Integer.parseInt(input);
+        img = new ImageIcon("toystory5.png");
+        
     }
         @Override
         public void paintComponent(Graphics g) {
-            //You custom rendering code
+            
+             if (age >= 0 && age <= 3) {
+                g.drawString ("You get in free!", 20, 20);
+                img.paintIcon (null, g, 10, 10); 
+                } else {}
         }
 
-}
 
 
 
@@ -25,3 +34,4 @@ public static void main(String[] args) {
         window.setVisible(true);
 
     }
+}
